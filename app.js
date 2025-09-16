e526aadf5dd849e9aa5989a110913b77d5f217c1
SIDNEY CAMPOS ARAGÃO <25132876@CFP127ALUNO.LOCAL>
2025-09-16 14:18:22 -0300

    Corrigindo e adicionando

--- app.js
+++ app.js
@@ -2,30 +2,53 @@
 
 const main = document.querySelector('main')
 const botaoBuscar = document.getElementById('buscar')
+const buscar = document.getElementById('entrada')
 
-function mostrarCachorro(cachorro) {
+
+
+// async -> porque vamos usar await dentro dela
+async function mostrarCachorro() {
+    const entrada = document.getElementById('entrada').value
+    
+    // Espera as imagens chegarem da API
+    const imagens = await buscarImagens(entrada)
+
+    // Limpa antes de adicionar
+    main.replaceChildren()
+
     const card = document.createElement('div')
-    const img = document.createElement('img')
-    
-    img.src = cachorro
-    
-    card.appendChild(img)
     card.classList.add('card-cachorro')
+
+    imagens.forEach((url) => {
+        const a = document.createElement('a')
+        const img = document.createElement('img')
+        img.src = url
+        a.href = url
+        a.setAttribute('target', '_blank')
+        a.appendChild(img)
+        card.appendChild(a)
+    })
+
     main.appendChild(card)
 }
+
+
+
 //async -> Função assincrona
 async function buscarImagens(breed) {
     const url = `https://dog.ceo/api/breed/${breed}/images`
-    //await -> aguardar a resposta dos dados antes de executa-lo
-    //fetch -> buscar
+    //await -> Aguardar a resposta dos dados antes de executa-lo
+    //fetch -> Buscar
     const response = await fetch(url)
     const imagens = await response.json()
     return imagens.message
 }
 
-botaoBuscar.addEventListener('click', async () => {
-    const entrada = document.getElementById('entrada').value
-    const imagens = await buscarImagens(entrada)
-    main.replaceChildren()
-    imagens.forEach(img => mostrarCachorro(img))
+botaoBuscar.addEventListener('click', mostrarCachorro)
+buscar.addEventListener('keydown', (event)=>{
+    if(event.key === 'Enter'){
+        mostrarCachorro()
+
+    }
 })
+
